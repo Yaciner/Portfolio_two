20a2113a50c77b7bdfe40ef80ea8fbb4f6cda92a
Yacine <yacine.redjala@dms.be>
2020-04-12 11:49:19 +0200

    Textured background

--- src/js/components/Homepage.jsx
+++ src/js/components/Homepage.jsx
@@ -1,12 +1,25 @@
 import React, {Component} from 'react';
 // import { Link } from 'react-router-dom';
+import Grained from '../lib/Grained';
+
 class Homepage extends Component {
 
-componentDidMount() {
+  componentDidMount() {
+    // setup the options for the bg
+    let options = {
+      animate: true,
+      patternWidth: 100,
+      patternHeight: 100,
+      grainOpacity: 0.03,
+      grainDensity: 1,
+      grainWidth: 1,
+      grainHeight: 1
+   };
+   // Initialize textured background animation
+      Grained('#bg',options);
+  }
 
-}
   render() {
-
     return (
       <div className="home">
       </div>
